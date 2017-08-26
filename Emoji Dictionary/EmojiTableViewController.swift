c160e8e44e9cdfe25e0565679c32ac95005ed981
Marco Grier <mlgrier@gmail.com>
2017-08-26 10:31:14 -0500

    Added a table view

---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Marco Grier on 8/26/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
