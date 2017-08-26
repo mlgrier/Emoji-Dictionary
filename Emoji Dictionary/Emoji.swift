a8f3f0d35f37a721e9f0a10371663b8e01275a8a
Unknown <mlgrier@gmail.com>
2017-08-26 18:45:59 -0500

    made a emoji class

---
+++ Emoji Dictionary/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Marco Grier on 8/26/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class Emoji {
+    var theEmoji = ""
+    var def = ""
+    var releaseDate = 0
+    var category = ""
+}
