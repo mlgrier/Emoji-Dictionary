d53a44fa3f2f263065b472fde8dd9ce8fda8f78f
Unknown <mlgrier@gmail.com>
2017-08-26 19:15:27 -0500

    Clean up the code

--- Emoji Dictionary/EmojiDefinitionViewController.swift
+++ Emoji Dictionary/EmojiDefinitionViewController.swift
@@ -26,33 +26,10 @@ class EmojiDefinitionViewController: UIViewController {
         birthLabel.text = "Release Date: \(emoji.releaseDate)"
         categoryLabel.text = "Category: \(emoji.category)"
 
-//        emojiLabel.text = emoji
-//
+
 //        // ["🥝", "🥃", "🍿", "🥓", "🏈", "👟", "🍔", "🍱", "🍘"]
-//
-//        if emoji == "🥝" {
-//            emojiDefinitionLabel.text = "Some food"
-//            birthLabel.text = "Birth Year: 2014"
-//            categoryLabel.text = "Category: Food"
-//        }
-//
-//        if emoji == "🥃" {
-//            emojiDefinitionLabel.text = "Cruise Drink"
-//            birthLabel.text = "Birth Year: 2012"
-//            categoryLabel.text = "Category: Drink"
-//        }
-//
-//        if emoji == "🍿" {
-//            emojiDefinitionLabel.text = "Popcorn"
-//            birthLabel.text = "Birth Year: 2011"
-//            categoryLabel.text = "Category: Snack"
-//        }
-//
-//        if emoji == "🥓" {
-//            emojiDefinitionLabel.text = "Bacon"
-//            birthLabel.text = "Birth Year: 2015"
-//            categoryLabel.text = "Category: Food"
-//        }
+
+
     }
 
 }
